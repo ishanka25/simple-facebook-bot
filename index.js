523cd702e2bb3f2bcec587b2a2666520e16daa10
ishanka25 <ishan.ranatunga@gmail.com>
2018-03-22 14:01:08 +0530

    added env port for heroku

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server is running on port ' + process.env.PORT));
